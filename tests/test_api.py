009dfd21115ef79a76a7df9f7b1b98ecf6bf0c5b
IGNACIO ROZAS LÓPEZ <Ignacio.Rozas@alu.uclm.es>
2024-11-12 20:31:32 +0100

    Prueba integracion con luideoz/Authentication

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,6 +6,7 @@ from time import sleep
 import unittest
 import requests
 from tokensrv import command_handlers as ch
+import pytest
 
 ADMIN_USERNAME = 'administrator'
 ADMIN_PASS_HASH = hashlib.sha256('adminpass'.encode()).hexdigest()
@@ -13,22 +14,32 @@ ADMIN_AUTH_CODE = hashlib.sha256(f'{ADMIN_USERNAME}{ADMIN_PASS_HASH}'.encode()).
 USER_USERNAME = 'user'
 USER_PASS_HASH = hashlib.sha256('userpass'.encode()).hexdigest()
 USER_PASS_CODE = hashlib.sha256(f'{USER_USERNAME}{USER_PASS_HASH}'.encode()).hexdigest()
-
-
+URI_AUTH = 'http://127.0.0.1:3000/auth/v1'
+#URI_AUTH = 'http://127.0.0.1:3001/api/v1'
 
 
 class TestApi(unittest.TestCase):
     """Tests for the API of the token server."""
 
+
+    @pytest.fixture(scope="session", autouse=True)
+    def setup_once():
+        # Coloca aquí el código que quieres ejecutar al inicio
+        print("Ejecutando setup al inicio de los tests")
+        with ch.make_server(URI_AUTH).test_client()  as client:
+            headers = {"Content-Type": "application/json"}
+            response = client.put('/auth/v1/user', json={'username':USER_USERNAME, 'password':USER_PASS_HASH, 'role':'user'}, headers=headers)
+            assert response.status_code == 400
+
     def test_alive_mock(self):
         """Test the alive endpoint."""
-        response = requests.get("http://127.0.0.1:3001/api/v1/alive",timeout=5)
+        response = requests.get(URI_AUTH+"/alive",timeout=5)
         #print(response.text)
         self.assertEqual(response.status_code, 204)
 
     def test_status(self):
         """Test the status endpoint."""
-        with ch.make_server("http://127.0.0.1:3001/api/v1").test_client()  as client:
+        with ch.make_server(URI_AUTH).test_client()  as client:
             service = client.application.config['service_token']
             response = client.get('/api/v1/status')
             self.assertEqual(response.status_code, 200)
@@ -36,7 +47,7 @@ class TestApi(unittest.TestCase):
 
     def test_make_token(self):
         """Test the make_token endpoint."""
-        server =ch.make_server("http://127.0.0.1:3001/api/v1")
+        server =ch.make_server(URI_AUTH)
         with server.test_client()  as client:
 
             service = client.application.config['service_token']
@@ -67,21 +78,21 @@ class TestApi(unittest.TestCase):
             #Compruebo que el token ha sido eliminado
             self.assertNotIn(token, service.tokens.keys())
     def test_make_token_BadRequest(self):
-        server =ch.make_server("http://127.0.0.1:3001/api/v1")
+        server =ch.make_server(URI_AUTH)
         with server.test_client()  as client:
             response = client.put('/api/v1/token',
                             json={"name":USER_USERNAME,
                             "pass":USER_PASS_HASH})
             self.assertEqual(response.status_code, 400)
     def test_make_token_Unauthorized(self):
-        server =ch.make_server("http://127.0.0.1:3001/api/v1")
+        server =ch.make_server(URI_AUTH)
         with server.test_client()  as client:
             response = client.put('/api/v1/token',
                             json={"username":USER_USERNAME,
                             "pass_hash":"bad_hash"})
             self.assertEqual(response.status_code, 401)
     def test_delete_token(self):
-        server =ch.make_server("http://127.0.0.1:3001/api/v1")
+        server =ch.make_server(URI_AUTH)
         with server.test_client()  as client:
             service = client.application.config['service_token']
             token,_ = service.make_token(USER_USERNAME)
@@ -91,7 +102,7 @@ class TestApi(unittest.TestCase):
             #Compruebo el codigo de respuesta
             self.assertEqual(response.status_code, 204)
     def test_delete_token_not_owner(self):
-        server =ch.make_server("http://127.0.0.1:3001/api/v1")
+        server =ch.make_server(URI_AUTH)
         with server.test_client()  as client:
             service = client.application.config['service_token']
             token,_ = service.make_token(USER_USERNAME)
@@ -101,7 +112,7 @@ class TestApi(unittest.TestCase):
             #Compruebo el codigo de respuesta
             self.assertEqual(response.status_code, 400)
     def test_delete_token_Forbidden(self):
-        server =ch.make_server("http://127.0.0.1:3001/api/v1")
+        server =ch.make_server(URI_AUTH)
         with server.test_client()  as client:
             service = client.application.config['service_token']
             token,_ = service.make_token(USER_USERNAME)
@@ -111,7 +122,7 @@ class TestApi(unittest.TestCase):
             #Compruebo el codigo de respuesta
             self.assertEqual(response.status_code, 401)
     def test_delete_token_not_found(self):
-        server =ch.make_server("http://127.0.0.1:3001/api/v1")
+        server =ch.make_server(URI_AUTH)
         with server.test_client()  as client:
             service = client.application.config['service_token']
             token,_ = service.make_token(USER_USERNAME)
@@ -122,7 +133,7 @@ class TestApi(unittest.TestCase):
             self.assertEqual(response.status_code, 404)
     def test_get_token(self):
         """Test the get_token endpoint."""
-        server =ch.make_server("http://127.0.0.1:3001/api/v1")
+        server =ch.make_server(URI_AUTH)
         with server.test_client()  as client:
             #crea un token sin expiration_cb
             service = client.application.config['service_token']
@@ -146,7 +157,7 @@ class TestApi(unittest.TestCase):
             #sleep(6)
     def test_get_token_not_found(self):
         """Test the get_token endpoint."""
-        server =ch.make_server("http://127.0.0.1:3001/api/v1")
+        server =ch.make_server(URI_AUTH)
         with server.test_client()  as client:
             #crea un token sin expiration_cb
             service = client.application.config['service_token']
