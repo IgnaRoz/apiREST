a653f6821816d0ccf336f601b1e8407a93982566
IGNACIO ROZAS LÓPEZ <Ignacio.Rozas@alu.uclm.es>
2025-01-12 16:46:22 +0100

    Cambio nombre metodo is_authorized

--- tokensrv/clientAuth.py
+++ tokensrv/clientAuth.py
@@ -27,7 +27,7 @@ class ClientAuth:
         try:
             # Calculate the authToken using SHA-256. auth_string = user + pass_hash
             auth_token = hashlib.sha256(f'{user}{pass_hash}'.encode()).hexdigest()
-            response = requests.get(f'{self.uri}/auth/{auth_token}', timeout=5)
+            response = requests.get(f'{self.uri}/is_authorized/{auth_token}', timeout=5)
             if response.status_code not in (200, 201,204):
                 return []
             return response.json()['roles']
